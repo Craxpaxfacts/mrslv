fa1e9061194a29a880334b43df90da0b233b5187
Craxpaxfacts <hidesense@gmail.com>
2025-09-24 22:18:25 +0300

    gl up

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,12 +11,14 @@ export const projects = [
     description:
       'An attempt to reach depth through orchestral & ambient music, exploring life and its moments. Each composition is a short story, where melody, harmony, and instrumental palette create their own mood and narrative. The music communicates beyond words, drawing the listener into a unique experience.',
     tracks: [
-      { cover: asset('/assets/mirtolk3.webp'), audio: asset('/assets/mirtolk3.mp3') },
-      { cover: asset('/assets/mirtolk4.webp'), audio: asset('/assets/mirtolk4.mp3') },
-      { cover: asset('/assets/mirtolk1.webp'), audio: asset('/assets/mirtolk1.mp3') },
-      { cover: asset('/assets/mirtolk1.webp'), audio: asset('/assets/mirtolk1.mp3') },
-      { cover: asset('/assets/mirtolk1.webp'), audio: asset('/assets/mirtolk1.mp3') },
-      { cover: asset('/assets/mirtolk2.webp'), audio: asset('/assets/mirtolk2.mp3') },
+      { cover: asset('/assets/blackswan.webp'), audio: asset('/assets/blackswan.mp3') },
+      { cover: asset('/assets/keepers.webp'), audio: asset('/assets/keepers.mp3') },
+      { cover: asset('/assets/illusion.webp'), audio: asset('/assets/illusion.mp3') },
+      { cover: asset('/assets/synesthesia.webp'), audio: asset('/assets/synesthesia.mp3') },
+      { cover: asset('/assets/sinkfade.webp'), audio: asset('/assets/sinkfade.mp3') },
+      { cover: asset('/assets/blackdogs.webp'), audio: asset('/assets/blackdogs.mp3') },
+      { cover: asset('/assets/waltz.webp'), audio: asset('/assets/waltz.mp3') },
+      { cover: asset('/assets/genesis.webp'), audio: asset('/assets/genesis.mp3') },
     ],
     links: [
       { src: asset('/assets/logos/spotify.svg'), href: 'https://spotify.com', alt: 'Spotify' },
@@ -34,12 +36,12 @@ export const projects = [
     description:
       'Pulsating techno rhythms, deep textures, and modern sound design create an immersive experience. The music is built on drive, dynamic energy, and a hypnotic atmosphere.',
     tracks: [
-      { cover: asset('/assets/mrslv.webp'), audio: asset('/assets/mrslv1.mp3') },
-      { cover: asset('/assets/mrslv2.webp'), audio: asset('/assets/mrslv5.mp3') },
-      { cover: asset('/assets/mrslv1.webp'), audio: asset('/assets/mrslv2.mp3') },
-      { cover: asset('/assets/mrslv.webp'), audio: asset('/assets/mrslv1.mp3') },
-      { cover: asset('/assets/mrslv2.webp'), audio: asset('/assets/mrslv5.mp3') },
-      { cover: asset('/assets/mrslv1.webp'), audio: asset('/assets/mrslv2.mp3') },
+      { cover: asset('/assets/architect.webp'), audio: asset('/assets/architect.mp3') },
+      { cover: asset('/assets/pushthelimit.webp'), audio: asset('/assets/pushthelimit.mp3') },
+      { cover: asset('/assets/offcontrol.webp'), audio: asset('/assets/offcontrol.mp3') },
+      { cover: asset('/assets/architect.webp'), audio: asset('/assets/architect.mp3') },
+      { cover: asset('/assets/pushthelimit.webp'), audio: asset('/assets/pushthelimit.mp3') },
+      { cover: asset('/assets/offcontrol.webp'), audio: asset('/assets/offcontrol.mp3') },
     ],
     links: [
       { src: asset('/assets/logos/spotify.svg'), href: 'https://spotify.com', alt: 'Spotify' },
@@ -57,12 +59,14 @@ export const projects = [
     description:
       'The project aims to approach music as a space without stylistic boundaries. Here, noise and melody, rhythm and silence intersect, creating different forms of sound. It is an exploration of sound and perception, where music becomes a means of exploration...',
     tracks: [
-      { cover: asset('/assets/cogvir.jpeg'), audio: asset('/assets/cogvir.mp3') },
-      { cover: asset('/assets/cogsll.jpg'), audio: asset('/assets/cogsll.wav') },
-      { cover: asset('/assets/cog3.png'), audio: asset('/assets/cognesthetic_3.mp3') },
-      { cover: asset('/assets/cogvir.jpeg'), audio: asset('/assets/cogvir.mp3') },
-      { cover: asset('/assets/cogsll.jpg'), audio: asset('/assets/cogsll.wav') },
-      { cover: asset('/assets/cog3.png'), audio: asset('/assets/cognesthetic_3.mp3') },
+      { cover: asset('/assets/brainwashing.webp'), audio: asset('/assets/brainwashing.mp3') },
+      { cover: asset('/assets/virus.webp'), audio: asset('/assets/virus.mp3') },
+      { cover: asset('/assets/ssl.webp'), audio: asset('/assets/ssl.mp3') },
+      { cover: asset('/assets/winteragain.webp'), audio: asset('/assets/winteragain.mp3') },
+      { cover: asset('/assets/brainwashing.webp'), audio: asset('/assets/brainwashing.mp3') },
+      { cover: asset('/assets/virus.webp'), audio: asset('/assets/virus.mp3') },
+      { cover: asset('/assets/ssl.webp'), audio: asset('/assets/ssl.mp3') },
+      { cover: asset('/assets/winteragain.webp'), audio: asset('/assets/winteragain.mp3') },
     ],
     links: [
       { src: asset('/assets/logos/spotify.svg'), href: 'https://spotify.com', alt: 'Spotify' },
